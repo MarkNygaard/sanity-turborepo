14bc55f3f45eade10f67b194ddc868bdb6bc52bf
Mark Nygaard <mark.nygaard@hotmail.com>
2025-06-09 14:40:07 +0200

    fix: switch to frontpage when changing language

--- apps/web/src/components/Layout/Footer/LanguageSelector.tsx
+++ apps/web/src/components/Layout/Footer/LanguageSelector.tsx
@@ -72,7 +72,10 @@ export default function LanguageSelector({
     }
 
     startTransition(() => {
-      router.replace({ pathname, params: { slug } }, { locale: nextLocale });
+      router.replace(
+        { pathname, params: { slug: "/" } },
+        { locale: nextLocale },
+      );
     });
   };
 
