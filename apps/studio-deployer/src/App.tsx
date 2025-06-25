712fdce24520faefb7172aeacd70faf5f9d6c6a3
Mark Nygaard Leth <mark.nygaard@hotmail.com>
2025-06-25 20:17:32 +0200

    fix: removed market list

--- apps/studio-deployer/src/App.tsx
+++ apps/studio-deployer/src/App.tsx
@@ -2,24 +2,16 @@ import { type SanityConfig } from "@sanity/sdk";
 import { SanityApp } from "@sanity/sdk-react";
 import { Flex, Spinner } from "@sanity/ui";
 
-import { MarketList } from "./components/MarketList";
 import { StudioDeploymentManager } from "./components/StudioDeploymentManager";
 import { SanityUI } from "./SanityUI";
 
 function App() {
-  // apps can access many different projects or other sources of data
   const sanityConfigs: SanityConfig[] = [
     {
       projectId: "y69aqpxn",
       dataset: "production",
     },
   ];
-
-  console.log(
-    "Sanity config:",
-    import.meta.env.VITE_SANITY_PROJECT_ID,
-    import.meta.env.VITE_SANITY_DATASET,
-  );
 
   function Loading() {
     return (
@@ -33,7 +25,6 @@ function App() {
     <SanityUI>
       <SanityApp config={sanityConfigs} fallback={<Loading />}>
         <StudioDeploymentManager />
-        {/* <MarketList /> */}
       </SanityApp>
     </SanityUI>
   );
